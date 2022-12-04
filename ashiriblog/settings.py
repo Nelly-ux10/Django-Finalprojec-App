580dfa04080f42b576361adbcad4f3b253f75f3a
Nelly-ux10 <77912139+Nelly-ux10@users.noreply.github.com>
2022-12-04 16:29:40 -0500

    debug on

--- ashiriblog/settings.py
+++ ashiriblog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5cy1k$l38d7)5+^f@92@=9!3dgmp+2a2psmfc42897@7$o5)vz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
